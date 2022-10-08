fc06aca3ee4ae60add2185b83dfa82678e55d3ca
bolayangnum1 <bolot.muxa@gmail.com>
2022-10-08 15:07:30 +0600

    jenkins setting

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,5 @@
-import os
 import pytest
 from selenium import webdriver
-import json
 
 
 def pytest_addoption(parser):
@@ -15,17 +13,17 @@ def get_webdriver(request):
     browser = request.config.getoption("--browser")
     executor = request.config.getoption('--executor')
     br_version = request.config.getoption('--br_version')
-    caps = {
+    capabilities = {
         "browserName": browser,
         "browserVersion": br_version,
         "selenoid:options": {
-            "enableVNC": True,
+            "enableVNC": False,
             "enableVideo": False
         }
     }
     driver = webdriver.Remote(
         command_executor=f'http://{executor}:4444/wd/hub',
-        desired_capabilities=caps,
+        desired_capabilities=capabilities
     )
     return driver
 
@@ -41,40 +39,39 @@ def setup(request, get_webdriver):
     driver.quit()
 
 
-@pytest.fixture(autouse=True)
-def create_allure_env():
-    environment = ['Browser=Chrome\n', 'Browser.Version=89.0.4389.128\n', 'Stand=Test\n', 'python.Version=3.7.9\n']
-    with open('allure/environment.properties', 'w+') as outfile:
-        for listitem in environment:
-            outfile.write(listitem)
-
-
-@pytest.fixture(autouse=True)
-def create_allure_category():
-    category = [
-      {
-        "name": "Игнорируемые тесты",
-        "matchedStatuses": ["пропущено"]
-      },
-      {
-        "name": "Проблемы с инфраструктурой",
-        "matchedStatuses": ["сломан", "сбой"],
-        "messageRegex": ".*signOut.*"
-      },
-      {
-        "name": "Устаревшие тесты",
-        "matchedStatuses": ["сломан"],
-        "traceRegex": ".*FileNotFoundException.*"
-      },
-      {
-        "name": "Дефекты товара",
-        "matchedStatuses": ["не удалось"]
-      },
-      {
-        "name": "Проверить дефекты",
-        "matchedStatuses": ["сломан"]
-      }
-    ]
-    with open('allure/category.json', 'w') as outfile:
-        json.dump(category, outfile)
-
+# @pytest.fixture(autouse=True)
+# def create_allure_env():
+#     environment = ['Browser=Chrome\n', 'Browser.Version=89.0.4389.128\n', 'Stand=Test\n', 'python.Version=3.7.9\n']
+#     with open('allure/environment.properties', 'w+') as outfile:
+#         for listitem in environment:
+#             outfile.write(listitem)
+#
+#
+# @pytest.fixture(autouse=True)
+# def create_allure_category():
+#     category = [
+#       {
+#         "name": "Игнорируемые тесты",
+#         "matchedStatuses": ["пропущено"]
+#       },
+#       {
+#         "name": "Проблемы с инфраструктурой",
+#         "matchedStatuses": ["сломан", "сбой"],
+#         "messageRegex": ".*signOut.*"
+#       },
+#       {
+#         "name": "Устаревшие тесты",
+#         "matchedStatuses": ["сломан"],
+#         "traceRegex": ".*FileNotFoundException.*"
+#       },
+#       {
+#         "name": "Дефекты товара",
+#         "matchedStatuses": ["не удалось"]
+#       },
+#       {
+#         "name": "Проверить дефекты",
+#         "matchedStatuses": ["сломан"]
+#       }
+#     ]
+#     with open('allure/category.json', 'w') as outfile:
+#         json.dump(category, outfile)
